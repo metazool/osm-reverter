088e97807645799785f073205bf3ae9cfb397f7c
Jo Walsh <metazool@fastmail.net>
2015-02-01 09:05:25 +0000

    stubs to match OsmApi.pm subs

---
+++ src/osm_reverter/osmapi.clj
@@ -0,0 +1,33 @@
+(ns osm-reverter.osmapi
+  (:gen-class))
+
+(defn get
+  [url]
+  )
+
+(defn get_with_credentials
+  [url user passwd]
+  )
+
+(defn add_to_discussion
+  [changeset comment]
+  )
+
+(defn put
+  [url body]
+  )
+
+(defn post
+  [url body]
+  )
+
+(defn delete
+  [url body]
+  )
+
+(defn debuglog
+  [request response]
+  )
+
+
+
